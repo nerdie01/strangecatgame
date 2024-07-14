3c86c9088f0c7f92bbfb25b9715bf60815c97bec
SimplyChicken <graylight10@gmail.com>
2024-07-14 13:44:04 -0400

    adwwad

--- scripts/stoveTop.gd
+++ scripts/stoveTop.gd
@@ -1,14 +1,17 @@
 extends Area2D
-@export var hp = 5
+#@export var hp = 5
 func _on_body_entered(body):
-	hp -= 1
-	wait(5)
-	print(hp)
+	#
+	body.get_parent().health -= 1
+	print(body.get_parent().health)
+	#hp -= 1
+	#wait(5)
+	#print(hp)
 	
-func _process(delta):
+#func _process(delta):
 	
-	if(hp <= 0):
-		hp = 5
-		get_tree().reload_current_scene()
-func wait(seconds: float) -> void:
-  await get_tree().create_timer(seconds).timeout		
+	#if(hp <= 0):
+		#hp = 5
+		#get_tree().reload_current_scene()
+#func wait(seconds: float) -> void:
+  #await get_tree().create_timer(seconds).timeout		
